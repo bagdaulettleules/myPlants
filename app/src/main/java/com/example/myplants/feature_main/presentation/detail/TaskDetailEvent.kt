54b36239a12f10bba27d71f76b783647f9c67976
Bagdaulet Tleules <bagdaulettleules@gmail.com>
2023-09-16 23:42:14 +0600

    refactored

--- app/src/main/java/com/example/myplants/feature_main/presentation/detail/TaskDetailEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myplants.feature_main.presentation.detail
-
-import com.example.myplants.feature_main.domain.model.Task
-
-sealed class TaskDetailEvent {
-    data class PageSelected(val page: Int) : TaskDetailEvent()
-    data class MarkWatered(val task: Task) : TaskDetailEvent()
-}
