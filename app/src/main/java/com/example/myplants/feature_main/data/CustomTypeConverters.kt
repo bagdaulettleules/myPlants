db36516658e2bd2485b38dfd2724bce206f527c8
Bagdaulet Tleules <bagdaulettleules@gmail.com>
2023-09-25 23:50:16 +0600

    feature notifications

--- app/src/main/java/com/example/myplants/feature_main/data/CustomTypeConverters.kt
+++ app/src/main/java/com/example/myplants/feature_main/data/CustomTypeConverters.kt
@@ -1,7 +1,6 @@
 package com.example.myplants.feature_main.data
 
 import androidx.room.TypeConverter
-import com.example.myplants.feature_main.domain.model.Size
 import com.example.myplants.feature_notification.domain.model.NotificationType
 import java.time.DayOfWeek
 
@@ -15,26 +14,6 @@ class CustomTypeConverters {
     @TypeConverter
     fun toDayOfWeekList(value: String): List<DayOfWeek> {
         return value.split(",").map { DayOfWeek.valueOf(it) }
-    }
-
-    @TypeConverter
-    fun fromSize(size: Size): String {
-        return size.name
-    }
-
-    @TypeConverter
-    fun toSize(name: String): Size {
-        return enumValueOf(name)
-    }
-
-    @TypeConverter
-    fun fromListString(string: List<String>): String {
-        return string.joinToString(",")
-    }
-
-    @TypeConverter
-    fun toListString(value: String): List<String> {
-        return value.split(",")
     }
 
     @TypeConverter
